143bec821eef8b6dd92dc25f792430f958141f37
atk <atksuimiy656@gmail.com>
2016-02-25 05:05:59 +0000

    make seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,81 @@
+Bgm.create([
+    {
+      track_id: 1003258781,
+      artist_id: 269598403,
+      track_name: 'SUN',
+      artist_name: '星野源',
+    },
+    {
+      track_id: 844001481,
+      artist_id: 211210469,
+      track_name: 'Ten',
+      artist_name: 'アフロジャック',
+    },
+    {
+      track_id: 1057566394,
+      artist_id: 41295873,
+      track_name: '未来',
+      artist_name: 'コブクロ',
+    },
+    {
+      track_id: 976353585,
+      artist_id: 430932944,
+      track_name: 'Do not look down',
+      artist_name: 'Martin',
+    },
+    {
+      track_id: 526041018,
+      artist_id: 1798556,
+      track_name: 'Payphone',
+      artist_name: 'マルーン5',
+    },
+    {
+      track_id: 961514348,
+      artist_id: 211210469,
+      track_name: 'Ten',
+      artist_name: 'アフロジャック',
+    },
+    {
+      track_id: 558731424,
+      artist_id: 558731421,
+      track_name: 'READY!',
+      artist_name: '765PRO',
+    },
+    {
+      track_id: 587762397,
+      artist_id: 411582529,
+      track_name: 'Snow',
+      artist_name: 'ミューズ',
+    }])
+    
+Post.create([
+    {
+      bgm_id: 7,
+      x: 34.6682738,
+      y: 138.9343542 
+    },
+    {
+      bgm_id: 8,
+      x: 36.100336,
+      y: 140.102528 
+    },
+    {
+      bgm_id: 7,
+      x: 36.100336,
+      y: 140.102528 
+    },
+    {
+      bgm_id: 7,
+      x: 35.6585696,
+      y: 139.745484 
+    },
+    {
+      bgm_id: 1,
+      x: 36.0856846,
+      y: 140.1080861
+    },
+    {
+      bgm_id: 1,
+      x: 35.6585696,
+      y: 139.745484 
+    }])
