73e51f6c6ce1146bd30b68f930518823cb97a65f
atk <atksuimiy656@gmail.com>
2016-02-27 06:46:24 +0000

    bgm.rb fix

--- models/bgm.rb
+++ models/bgm.rb
@@ -1,2 +1,13 @@
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL']||"sqlite3:db/development.db")
-class Bgm < ActiveRecord::Base; end
+class Bgm < ActiveRecord::Base
+    has_many :posts    
+end
+
+class Post < ActiveRecord::Base
+    belongs_to :bgm
+    has_many :users
+end
+
+class User < ActiveRecord::Base
+    belongs_to :post
+end
